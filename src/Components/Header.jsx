b827d6bac2b56d20a196e001323fd190ab14ea95
Meerahbel <mirabelugoji@gmail.com>
2023-12-18 18:13:12 -0800

    dropdown designed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -21,29 +21,47 @@ const Header = () => {
       </div>
 
       {dropdown && (
-        <div className="absolute z-10 top-[70px] left-[50%] translate-x-[-50%]">
-          <ul className="flex flex-col items-center gap-5 text-black/60 bg-white w-[50%] text-2xl font-medium">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/work">Work</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact"> Contact</Link>
-            </li>
-          </ul>
-          <div className="flex flex-col gap-10">
-            <h1>Let's find solutions together?</h1>
-            <p>
-              Use my contacts below. You need to put a project on the move,
-              let’s work on it!
+        <div className="flex flex-col fixed w-full top-0 left-0 min-h-[100vh] bg-blue-600 py-20 px-5 md:px-40 z-[999]">
+          <div className="flex justify-between px-5 absolute w-full top-10 lg:top-5 left-0">
+            <p className="text-sm text-purple-800 font-bold tracking-widest">
+              Mirabel Ugoji.
             </p>
-            <p>mirabelugoji@gmail.com</p>
-            <button>Contact Me</button>
+            <div onClick={handleMenu}>
+              <img
+                src="/images/cancel.png"
+                alt="cancel icon"
+                className="w-6 h-6"
+              />
+            </div>
+          </div>
+          <div className="flex mt-10 md:mt-0">
+            <ul className="flex flex-col gap-8 md:gap-14 w-[50%] font-medium">
+              <li className="text-4xl font-semibold">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="text-4xl font-semibold">
+                <Link to="/work">Work</Link>
+              </li>
+              <li className="text-4xl font-semibold">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="text-4xl font-semibold">
+                <Link to="/contact"> Contact</Link>
+              </li>
+            </ul>
+            <div className="lg:flex flex-col gap-10 hidden">
+              <h1 className="text-4xl font-bold w-[80%]">
+                Let's find solutions together?
+              </h1>
+              <p className="w-[80%]">
+                Use my contacts below. You need to put a project on the move,
+                let’s work on it!
+              </p>
+              <p>mirabelugoji@gmail.com</p>
+              <button className="border-2 border-black w-[30%] py-1">
+                Contact Me
+              </button>
+            </div>
           </div>
         </div>
       )}
