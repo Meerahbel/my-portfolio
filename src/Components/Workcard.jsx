51a06a4ba78fb067f513fcc9c17d29cf91b43582
Meerahbel <mirabelugoji@gmail.com>
2024-01-07 17:22:45 -0800

    grayscale and bounce animation

--- src/Components/Workcard.jsx
+++ src/Components/Workcard.jsx
@@ -1,10 +1,25 @@
+import React, { useState } from "react";
+import "../Workcard.css";
+
 const Workcard = ({ item, index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+  };
+
   return (
-    <div className="flex flex-col gap-3 items-center my-10">
+    <div
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+      className="flex flex-col gap-3 items-center my-10 w-fit mx-auto"
+    >
       <img
         src={item?.image}
         alt="work image"
-        className="w-[400px] md:w-[550px] lg:w-[700px] h-[300px] md:h-[250px] lg:h-[300px]"
+        className={`w-[400px] md:w-[550px] lg:w-[700px] h-[300px] md:h-[250px] lg:h-[300px] image ${
+          isHovered ? "colored-image" : "bw-image"
+        }`}
       />
       <p className="font-bold">{item?.title}</p>
     </div>
