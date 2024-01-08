13db85e83327772b5d293a72988b4d79452b08a7
Meerahbel <mirabelugoji@gmail.com>
2024-01-07 16:46:29 -0800

    work page responsiveness

--- src/Components/Workcard.jsx
+++ src/Components/Workcard.jsx
@@ -1,7 +1,11 @@
 const Workcard = ({ item, index }) => {
   return (
     <div className="flex flex-col gap-3 items-center my-10">
-      <img src={item?.image} alt="work image" className="w-[700px] h-[300px]" />
+      <img
+        src={item?.image}
+        alt="work image"
+        className="w-[400px] md:w-[550px] lg:w-[700px] h-[300px] md:h-[250px] lg:h-[300px]"
+      />
       <p className="font-bold">{item?.title}</p>
     </div>
   );
